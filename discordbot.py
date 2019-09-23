a7e631ca68f66a3269116f98080462740fa956e3
hasu4238 <55648206+hasu4238@users.noreply.github.com>
2019-09-24 01:23:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,17 +1,20 @@
+import discord
 from discord.ext import commands
+import asyncio
+
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
 
