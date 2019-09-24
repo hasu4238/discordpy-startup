7d46d887cb47cd9d1d7a4cd5e025d5b776dd9627
hasu4238 <55648206+hasu4238@users.noreply.github.com>
2019-09-25 01:05:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -74,7 +74,7 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
                 else:
                     pass
         # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
-        await msg.remove_reaction(str(reaction.emoji), user)
+       # await msg.remove_reaction(str(reaction.emoji), user)
 
 
 client.run(token)
