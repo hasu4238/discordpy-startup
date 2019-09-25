d7e9a9c76b82958e46e16419eadf3f92268c4256
hasu4238 <55648206+hasu4238@users.noreply.github.com>
2019-09-26 01:03:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,24 +5,20 @@ from datetime import datetime, timedelta
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='.')
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('po2ng')
+async def neko(ctx):
+    await ctx.send('にゃーん')
     
 @client.event
 async def on_voice_state_update(member, before, after): 
-    if member.guild.id == 監視するサーバーid and (before.channel != after.channel):
+    if member.guild.id == 587605345942372352 and (before.channel != after.channel):
         now = datetime.utcnow() + timedelta(hours=9)
-        alert_channel = client.get_channel(通知させたいテキストチャンネルid)
+        alert_channel = client.get_channel(626439869039771668)
         if before.channel is None: 
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
